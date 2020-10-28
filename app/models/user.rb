b4465419301f6f2ba1fe279e4a7fd7c8f18adde3
Koju Sakaguchi <fuetf@icloud.com>
2020-10-28 15:27:46 +0900

    user_name registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates_uniqueness_of :user_name
+  validates_presence_of :user_name
+
 end
