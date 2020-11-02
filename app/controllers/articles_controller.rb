b098eb9a7b2f4247302b8d8188ba546dfb743022
Koju Sakaguchi <fuetf@icloud.com>
2020-11-02 15:01:00 +0900

    strong parameter picture 追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,25 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: 'Successful Submit!!'
+    else 
+      flash.now[:error] = 'failed Submit'
+      render :new
+    end
   end
+
+  private
+    def article_params
+        params.require(:article).permit(
+          :title, 
+          :reason,
+          :necessary_stuff,
+          :expiration,
+          :picture,
+          :achievement_flag
+        )
+    end
 
 end
