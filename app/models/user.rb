2d3b9efe05bc1100d3fdccd50df4fb8a0bf06d65
Koju Sakaguchi <fuetf@icloud.com>
2020-11-05 15:35:43 +0900

    following_id&follower_id relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,12 @@ class User < ApplicationRecord
   has_many :favorite_articles, through: :likes, source: :article
   has_one :profile, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :location, :birthday, to: :profile, allow_nil: true
   
   validates_uniqueness_of :user_name
@@ -40,6 +46,15 @@ class User < ApplicationRecord
 
   def has_liked?(article)
     likes.exists?(article_id: article.id)
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relationship = following_relationships.find_by!(following_id: user.id)
+    relationship.destroy!
   end
 
   def prepare_profile
