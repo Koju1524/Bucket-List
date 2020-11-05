018de638e6dbb27f8d43d96313e2f8a097c37c72
Koju Sakaguchi <fuetf@icloud.com>
2020-11-05 14:28:06 +0900

    action-text 設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_050136) do
+ActiveRecord::Schema.define(version: 2020_11_05_051440) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2020_11_05_050136) do
     t.integer "user_id", null: false
     t.string "title", null: false
     t.text "reason", null: false
-    t.text "necessary_stuff", null: false
     t.date "expiration", null: false
     t.integer "achievement_flag", null: false
     t.datetime "created_at", precision: 6, null: false
