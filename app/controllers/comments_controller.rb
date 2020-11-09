9e2759b797e1139d21f70daab801e91889bf2650
Koju Sakaguchi <fuetf@icloud.com>
2020-11-09 14:53:37 +0900

    comment controller& html&css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     article = Article.find(params[:article_id])
@@ -16,11 +17,25 @@ class CommentsController < ApplicationController
     end
   end
 
-  
+  def edit
+    article = Article.find(params[:article_id])
+    @comment = article.comments.find_by(id: params[:id])
+  end
+
+  def update
+    article = Article.find(params[:article_id])
+    @comment = article.comments.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      redirect_to article_path(article), notice: 'Successful Edit !!'
+    else
+      flash.now[:error] = 'Failed Edit'
+      render :edit
+    end
+  end
 
   def destroy
     article = Article.find(params[:article_id])
-    comment = article.comments(id: comment.id)
+    comment = article.comments.find_by(id: params[:id])
     comment.destroy!
     redirect_to article_path(article), notice: 'Deleted Comment'
   end
--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -11,5 +11,4 @@ module ArticleDecorator
   def like_count
     likes.count
   end
-  
 end
