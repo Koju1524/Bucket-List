6ae2600b48caaa50d7427f51851c83b95a91e71f
Koju Sakaguchi <fuetf@icloud.com>
2020-11-14 14:45:27 +0900

    following controller&html&css

---
+++ app/controllers/followings_controller.rb
@@ -0,0 +1,9 @@
+class FollowingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    user = User.find(params[:account_id])
+    @followings = user.followings
+  end
+  
+end
