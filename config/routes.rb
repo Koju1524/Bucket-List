c5166b2845f68f057fa86160239800fc7d168148
Koju Sakaguchi <fuetf@icloud.com>
2020-12-09 13:47:42 +0900

    routes&css modification

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resource :like, only: [:show, :create, :destroy]
     resources :achieved_articles, only: [:index, :show, :new, :create, :update, :edit] do
       resources :comments, only: [:index, :new, :create, :update, :edit, :destroy]
-      resource :thumbs_up, only: [:show, :create, :destroy]
+      resource :thumbs_up, only: [:create, :destroy]
     end
   end
 
