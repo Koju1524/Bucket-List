55eb9b5f86b49dd8a94a2c0388f89f9ccfcb5f1e
Koju Sakaguchi <fuetf@icloud.com>
2020-12-13 10:29:17 +0900

    like-model modificition

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   let!(:user) { create(:user) }
   let!(:article) { create(:article, user: user) }
+  let!(:like) { build(:like, article: article, user: user) }
 
-  context 'articleがある場合' do
-
-    like = article.likes.create(user: user, article: article)
-
-    it 'likeできる' do
+  context 'about like' do
+    it 'can like' do
       expect(like).to be_valid
     end
-
   end
 
 end
