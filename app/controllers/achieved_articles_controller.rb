2c04388909f3f361c0c5f797bfd80a3e6b12192a
Koju Sakaguchi <fuetf@icloud.com>
2020-12-14 16:44:55 +0900

    controller-flash 日本語化

--- app/controllers/achieved_articles_controller.rb
+++ app/controllers/achieved_articles_controller.rb
@@ -15,10 +15,10 @@ class AchievedArticlesController < ApplicationController
     @achieved_article = AchievedArticle.find_by(article_id: @article.id, user_id: @article.user_id)
     if params[:achieved_article][:title].present? && params[:achieved_article][:impression].present? && params[:achieved_article][:advice].present? && params[:achieved_article][:achieved_day].present? && params[:achieved_article][:images].present?
       if @achieved_article.update(achieved_article_params)
-        redirect_to article_achieved_article_path(@article, @achieved_article), notice: 'Congratulation!!'
+        redirect_to article_achieved_article_path(@article, @achieved_article), notice: 'おめでとう!!'
       end
     else
-      flash.now[:error] = 'Fill in the blank'
+      flash.now[:error] = '空欄を埋めてください!'
       render :edit
     end
   end
