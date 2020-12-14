c09aef25f6c0fede6862b6fe4908f59a8c43d7a2
Koju Sakaguchi <fuetf@icloud.com>
2020-12-14 18:49:58 +0900

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module BucketList
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
   end
 end
