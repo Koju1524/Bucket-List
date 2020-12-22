6d11191965ecb1ee16d664f1ab5ac461ca8f3cbc
Koju Sakaguchi <fuetf@icloud.com>
2020-12-22 13:22:39 +0900

    system achieved_article

--- spec/system/achieved_article_spec.rb
+++ spec/system/achieved_article_spec.rb
@@ -4,15 +4,12 @@ RSpec.describe 'achieved_article', type: :system do
   let!(:user) { create(:user) }
   let!(:article) { create(:article, achievement_flag: 'Achievement', user: user) }
   let!(:achieved_articles) { create_list(:achieved_article, 3, article: article, user: user) }
-  before do
-    @achieved_article = build(:achieved_article)
-    @achieved_article.images= fixture_file_upload('app/assets/images/beach.jpg')
-  end
 
   it 'achieved_articles-index' do
     visit article_achieved_articles_path(article_id: article.id)
 
     achieved_articles.each do |achieved_article|
+      Rack::Test::UploadedFile.new('app/assets/images/beach.jpg','images/jpg')
       expect(page).to have_css('.bucket_list', text: achieved_article.title)
     end
   end
